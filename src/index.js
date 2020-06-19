a656d6f5072c4ed1778efff3e53c60322a3fc85a
prince <prince18pat@gmail.com>
2020-06-19 23:03:45 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+require('./models/User');
+require('./models/Track');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
+const requireAuth = require('./middlewares/requireAuth');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
+
+const mongoUri = 'mongodb://root:example@localhost:27017'
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('connected to mongo instance');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('Error connecting to mongo', err);
+});
+
+
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Your email : ${req.user.email}`);
+});
+
+app.listen(3000, () => {
+  console.log('listening on 3000');
+})
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const User = mongoose.model('User');
+
+const router = express.Router();
+
+router.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = new User({ email, password });
+    await user.save();
+    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+    console.log(token);
+    res.send({ token: token });
+  } catch (err) {
+    console.log(err);
+    return res.status(422).send(err)
+  }
+});
+
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'Must provide email and password' });
+  }
+
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    res.status(422).send({ error: 'Invalid password or email' })
+  }
+
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+    res.send({ token: token });
+  } catch (err) {
+    res.status(422).send({ error: 'Invalid password or email' })
+  }
+
+})
+
+module.exports = router;
